2a43a8dc9276cc75db281f549c4be555794e6c5a
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-20 15:56:04 -0500

    rename to reference

--- Desarrollo/DKCommerceBussinesEntity/ProveedorBE.cs
+++ Desarrollo/DKCommerceBussinesEntity/ProveedorBE.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DKCommerce_Bussines_Entity
+namespace DKCommerceBussinesEntity
 {
     public class ProveedorBE
     {
