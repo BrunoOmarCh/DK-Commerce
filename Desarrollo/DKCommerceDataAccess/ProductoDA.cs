d559d93ccba593588213e98d1ab981180f02d096
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-21 16:32:22 -0500

    add Status into DataAccess Producto

--- Desarrollo/DKCommerceDataAccess/ProductoDA.cs
+++ Desarrollo/DKCommerceDataAccess/ProductoDA.cs
@@ -318,6 +318,41 @@ namespace DKCommerceDataAccess
             }
         }
 
+        public void Status(int idProducto, ProductoBE beProducto)
+        {
+            var conn = Configuration.GetConnectionString("DK_Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = UpProductoStatus;
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@IdProducto", SqlDbType.Int).Value = idProducto;
+                            sqlCmd.Parameters.Add("@Suspendido", SqlDbType.Bit).Value = beProducto.Suspendido;
+
+                        }
+                        catch (Exception)
+                        {
+                            sqlTran?.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+
 
     }
 }
