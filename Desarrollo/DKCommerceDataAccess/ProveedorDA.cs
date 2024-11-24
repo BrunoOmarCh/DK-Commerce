7510f27913d1f8e683b6258f19aaceda21179bba
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-23 19:26:56 -0500

    feat: implementar funcionalidad para actualizar registros de Proveedor

--- Desarrollo/DKCommerceDataAccess/ProveedorDA.cs
+++ Desarrollo/DKCommerceDataAccess/ProveedorDA.cs
@@ -153,5 +153,93 @@ namespace DKCommerceDataAccess
             }
         }
 
+        public void Update(int idProveedor, ProveedorBE beProveedor)
+        {
+            var conn = Configuration.GetConnectionString("DK_Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = UpProveedorUpdate;
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@ProveedorId", SqlDbType.Int).Value = idProveedor;
+
+                            sqlCmd.Parameters.Add("@Nombre", SqlDbType.NVarChar).Value = beProveedor.Nombre;
+
+                            if (beProveedor.Ruc != null)
+                                sqlCmd.Parameters.Add("@Ruc", SqlDbType.Char).Value = beProveedor.Ruc;
+                            else
+                                sqlCmd.Parameters.Add("@Ruc", SqlDbType.Char).Value = DBNull.Value;
+
+                            if (beProveedor.ContactoId != 0)
+                                sqlCmd.Parameters.Add("@ContactoId", SqlDbType.Int).Value = beProveedor.ContactoId;
+                            else
+                                sqlCmd.Parameters.Add("@ContactoId", SqlDbType.Int).Value = DBNull.Value;
+
+                            if (beProveedor.Direccion != null)
+                                sqlCmd.Parameters.Add("@Dirección", SqlDbType.NVarChar).Value = beProveedor.Direccion;
+                            else
+                                sqlCmd.Parameters.Add("@Dirección", SqlDbType.NVarChar).Value = DBNull.Value;
+
+                            if (beProveedor.Ciudad != null)
+                                sqlCmd.Parameters.Add("@Ciudad", SqlDbType.Char).Value = beProveedor.Ciudad;
+                            else
+                                sqlCmd.Parameters.Add("@Ciudad", SqlDbType.Char).Value = DBNull.Value;
+
+                            if (beProveedor.Region != null)
+                                sqlCmd.Parameters.Add("@Región", SqlDbType.NVarChar).Value = beProveedor.Region;
+                            else
+                                sqlCmd.Parameters.Add("@Región", SqlDbType.NVarChar).Value = DBNull.Value;
+
+                            if (beProveedor.CodPostal != null)
+                                sqlCmd.Parameters.Add("@CodPostal", SqlDbType.NVarChar).Value = beProveedor.CodPostal;
+                            else
+                                sqlCmd.Parameters.Add("@CodPostal", SqlDbType.NVarChar).Value = DBNull.Value;
+
+                            if (beProveedor.Pais != null)
+                                sqlCmd.Parameters.Add("@Pais", SqlDbType.NVarChar).Value = beProveedor.Pais;
+                            else
+                                sqlCmd.Parameters.Add("@Pais", SqlDbType.NVarChar).Value = DBNull.Value;
+
+                            if (beProveedor.Telefono != null)
+                                sqlCmd.Parameters.Add("@Telefono", SqlDbType.VarChar).Value = beProveedor.Telefono;
+                            else
+                                sqlCmd.Parameters.Add("@Telefono", SqlDbType.VarChar).Value = DBNull.Value;
+
+                            if (beProveedor.Fax != null)
+                                sqlCmd.Parameters.Add("@Fax", SqlDbType.VarChar).Value = beProveedor.Fax;
+                            else
+                                sqlCmd.Parameters.Add("@Fax", SqlDbType.VarChar).Value = DBNull.Value;
+
+                            if (beProveedor.PaginaPrincipal != null)
+                                sqlCmd.Parameters.Add("@PaginaPrincipal", SqlDbType.VarChar).Value = beProveedor.PaginaPrincipal;
+                            else
+                                sqlCmd.Parameters.Add("@PaginaPrincipal", SqlDbType.VarChar).Value = DBNull.Value;
+
+                            sqlCmd.ExecuteNonQuery();
+                            sqlTran.Commit();
+
+                        }
+                        catch (Exception)
+                        {
+                            sqlTran?.Rollback();
+                            throw;
+                        }
+
+                    }
+                }
+            }
+        }
+
+
     }
 }
