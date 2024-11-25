2f6bedf5535436ca2974556fe4dc7faba4e5d93b
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-24 20:20:22 -0500

    feat: agregar registro de Proveedores

--- Desarrollo/DKCommerceBussinesLogic/ProveedorBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ProveedorBL.cs
@@ -26,5 +26,19 @@ namespace DKCommerceBussinesLogic
             }
         }
 
+        public void Insert(ProveedorBE beProveedor)
+        {
+            try
+            {
+                var daProveedor = new ProveedorDA();
+                daProveedor.Insert(beProveedor);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
     }
 }
