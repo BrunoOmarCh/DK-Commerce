f597430639ac16d983bb1a362ca5b13c6a01a491
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-26 23:50:39 -0500

    Add CategoriaBL class with SelectById and Insert methods

--- Desarrollo/DKCommerceBussinesLogic/CategoriaBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/CategoriaBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace DKCommerceBussinesLogic
 {
-    class CategoriaBL
+    public class CategoriaBL
     {
+        public CategoriaBE SelectById(int categoriaId)
+        {
+            var daCategoria = new CategoriaDA();
+
+            return daCategoria.SelectById(categoriaId);
+        }
+
+        public void Insert(CategoriaBE beCategoria)
+        {
+            var daCategoria = new CategoriaDA();
+
+            daCategoria.Insert(beCategoria);
+        }
     }
 }
