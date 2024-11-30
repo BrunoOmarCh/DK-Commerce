f2094fbc857ac7701206005447a24c49c95a2dd0
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-29 23:54:26 -0500

    Add MapperConfiguiration

---
+++ Desarrollo/DKCommerceUI/Controllers/ProductoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using DKCommerceUI.Models;
+
+
+
+namespace DKCommerceUI.Controllers
+{
+    [Route("producto")]
+
+    public class ProductoController : Controller
+    {
+        private readonly IMapper _mapper;// Se define una interfaz para realizar el mapeo
+
+        public ProductoController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+    }
+}
--- Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
+++ Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eeb98b99c26278a2d7be587a9b5768896dd671e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e8f03a9d0584e1b0ae1eaedf2848343c1f81ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
