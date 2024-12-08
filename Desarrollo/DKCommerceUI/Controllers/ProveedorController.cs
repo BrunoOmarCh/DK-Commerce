12424903cb398786f04b46375088ce440ecd46bd
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-07 21:14:08 -0500

    add editar into ProveedorController

--- Desarrollo/DKCommerceUI/Controllers/ProveedorController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ProveedorController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Libreria;
 using MercurioUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace DKCommerceUI.Controllers
 {
@@ -31,6 +34,45 @@ namespace DKCommerceUI.Controllers
         public IActionResult Nuevo()
         {
             return View();
+        }
+        [HttpGet]
+        [Route("editar/{idProveedor}")]
+        public async Task<IActionResult> Editar(int idProveedor)
+        {
+            ViewBag.ProveedorId = idProveedor;
+
+            var dtoProveedor = new ProveedorModel();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("PlutonApi"));
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res = await client.GetAsync("api/proveedor/select-by-id/" + idProveedor + "/");
+                if (res.IsSuccessStatusCode)
+                {
+                    var mainProductoResult = await res.Content.ReadAsStringAsync();
+                    var beProveedor = JsonConvert.DeserializeObject<Proveedor>(mainProductoResult);
+
+                    dtoProveedor = _mapper.Map<ProveedorModel>(beProveedor);
+                }
+            }
+
+            ViewBag.Proveedor = dtoProveedor;
+            ViewBag.Nombre = dtoProveedor.Nombre;
+            ViewBag.Ruc = dtoProveedor.Ruc;
+            ViewBag.ContactoId = dtoProveedor.ContactoId;
+            ViewBag.Direccion = dtoProveedor.Direccion;
+            ViewBag.Ciudad = dtoProveedor.Ciudad;
+            ViewBag.Region = dtoProveedor.Region;
+            ViewBag.CodPostal = dtoProveedor.CodPostal;
+            ViewBag.Pais = dtoProveedor.Pais;
+            ViewBag.Telefono = dtoProveedor.Telefono;
+            ViewBag.Fax = dtoProveedor.Fax;
+            ViewBag.PaginaPrincipal = dtoProveedor.PaginaPrincipal;
+
+            return View(dtoProveedor);
         }
 
     }
