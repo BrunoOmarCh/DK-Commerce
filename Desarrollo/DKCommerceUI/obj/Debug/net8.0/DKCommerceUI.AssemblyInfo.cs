d613879c33c218a85a5929290682bd110a13d668
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-07 21:10:45 -0500

    Add Provedor Controller

--- Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
+++ Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6ea2a9e703e9939a39db255f778887b58d3be8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a531fa7ad30feaab43b58d1fd51abb95e18ec6b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
