c3bd837ebedfa25622d64f4082cd2e82ffaad8e2
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-09 23:53:09 -0500

    Add index.js into proveedor

--- Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
+++ Desarrollo/DKCommerceUI/obj/Debug/net8.0/DKCommerceUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdc89f31bf32b50c46385da83d42de9e91689f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d038cd2902a91ba069fcef5e93685138e8566826")]
 [assembly: System.Reflection.AssemblyProductAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DKCommerceUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
