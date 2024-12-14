38c9e038d7794db3a6e0f3fa3b72f317c1c17840
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-14 18:09:25 -0500

    add atributos

--- Desarrollo/DKCommerceUI/Models/CategoriaModel.cs
+++ Desarrollo/DKCommerceUI/Models/CategoriaModel.cs
@@ -2,5 +2,9 @@
 {
     public class CategoriaModel
     {
+        public int CategoriaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Suspendido { get; set; }
     }
 }
