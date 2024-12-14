127f9f09ad8118c339331fc38456d3578f45c41c
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-14 17:58:04 -0500

    add route

---
+++ Desarrollo/DKCommerceUI/Controllers/CategoriaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceUI.Controllers
+{
+    [Route("categoria")]
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
