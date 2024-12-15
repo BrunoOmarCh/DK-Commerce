b4bf5f69d27c111268ed8af532d1ac296e10e6fe
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-15 18:55:09 -0500

    Add references

--- Desarrollo/DKCommerceAPI/Controllers/CategoriaController.cs
+++ Desarrollo/DKCommerceAPI/Controllers/CategoriaController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceBussinesLogic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceAPI.Controllers
 {
     [ApiController]
     [Route("api/categoria")]
     public class CategoriaController : ControllerBase
-    { 
-    
+    {
+        [HttpGet]
+        [Route("select-by-id/{categoriaId}")]
+        public CategoriaBE SelectById(int categoriaId)
+        {
+            var blCategoria = new CategoriaBL();
+
+            return blCategoria.SelectById(categoriaId);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public void Insert(CategoriaBE beCategoria)
+        {
+            var blCategoria = new CategoriaBL();
+
+            blCategoria.Insert(beCategoria);
+        }
     }
-
-
 }
