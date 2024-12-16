6619809ab9d5b04fe85eba44e6915412b22af992
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-16 14:08:40 -0500

    Modificacion API

--- Desarrollo/DKCommerceAPI/obj/Debug/net8.0/DKCommerceAPI.AssemblyInfo.cs
+++ Desarrollo/DKCommerceAPI/obj/Debug/net8.0/DKCommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DKCommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e557891152ea227397d1feee335e575e12094562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d0837714656190673ed4acba55b235c6f7ba58")]
 [assembly: System.Reflection.AssemblyProductAttribute("DKCommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DKCommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
