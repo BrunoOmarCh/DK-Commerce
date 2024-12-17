06b3dfffbd8bdb0690824723738f157d71a6fe68
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-17 17:33:56 -0500

    Add Libreria

--- Desarrollo/Libreria/obj/Debug/net8.0/Libreria.AssemblyInfo.cs
+++ Desarrollo/Libreria/obj/Debug/net8.0/Libreria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eeb98b99c26278a2d7be587a9b5768896dd671e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02a175e256481a720a2b1017bda338ee6f2aaa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libreria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
