327a2c0b73b954950af93838db8e95f47341c2ab
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-21 17:25:33 -0500

    Ejecucion de select by id en Bussines Logic

--- Desarrollo/DKCommerceBussinesLogic/ContactoProveedorBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ContactoProveedorBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace DKCommerceBussinesLogic
 {
     public class ContactoProveedorBL
     {
+        public ContactoProveedorBE SelectById(int idContactoProveedor)
+        {
+            try
+            {
+                var daContactoProveedor = new ContactoProveedorDA();
+                ContactoProveedorBE beContactoProveedor;
+
+                beContactoProveedor = daContactoProveedor.SelectById(idContactoProveedor);
+                return beContactoProveedor;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
