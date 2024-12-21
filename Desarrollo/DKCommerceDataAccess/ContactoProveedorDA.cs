d689ad7438ea751e3c22f82d4b14d6ffa6d387c2
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-21 16:37:39 -0500

    Añadir procedimientos al DataAccess

--- Desarrollo/DKCommerceDataAccess/ContactoProveedorDA.cs
+++ Desarrollo/DKCommerceDataAccess/ContactoProveedorDA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Libreria;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DKCommerceDataAccess
 {
-    internal class ContactoProveedorDA
+    public class ContactoProveedorDA: ConfigDataAccess
     {
+        public const string UpProveedorInsert = "UpContactoProveedorInsert";
+        public const string UpProveedorUpdate = "UpContactoProveedorUpdate";
+        public const string UpProveedorDelete = "UpContactoProveedorDelete";
+        public const string UpProveedorSelById = "UpContactoProveedorSelById";
+
     }
 }
