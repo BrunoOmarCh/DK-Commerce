09ea608d55624adcde14f9cb4bd6443a81dcb17b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-20 23:36:21 -0500

    modificacion a ContactoProveedorBE

--- Desarrollo/DKCommerceBussinesEntity/ContactoProveedorBE.cs
+++ Desarrollo/DKCommerceBussinesEntity/ContactoProveedorBE.cs
@@ -9,7 +9,7 @@ namespace DKCommerceBussinesEntity
     public class ContactoProveedorBE
     {
         public int ContactoProveedorId { get; set; }  
-        public string NombreContacto{ get; set; }
+        public string? NombreContacto{ get; set; }
         public string? CargoContacto { get; set; }   
         public string? DNI {  get; set; }
 
