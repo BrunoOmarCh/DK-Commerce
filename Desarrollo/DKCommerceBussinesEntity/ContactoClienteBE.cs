cafe10fb65e08fe8694ef416d9f0368f724dabc3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-20 23:33:56 -0500

    Add restriccion Contacto Cliente

--- Desarrollo/DKCommerceBussinesEntity/ContactoClienteBE.cs
+++ Desarrollo/DKCommerceBussinesEntity/ContactoClienteBE.cs
@@ -9,7 +9,7 @@ namespace DKCommerceBussinesEntity
     public class ContactoCliente
     {
         public int ContactoId { get; set; }
-        public string NombreContacto { get; set; }
+        public string? NombreContacto { get; set; }
         public string? CargoContacto { get; set; }
         public string? TipoDocumento { get; set; }
         public string? NroDocumento { get; set; }
