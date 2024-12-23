75ae8640384cb867c517dd952de5d1949a276e5b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-23 00:49:03 -0500

    Add ContactoProveedorController

---
+++ Desarrollo/DKCommerceAPI/Controllers/ContactoProveedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceAPI.Controllers
+{
+    public class ContactoProveedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
