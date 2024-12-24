019c7bd8643da129d5f2015f0541f07f55f6b70e
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-24 00:16:19 -0500

    Add Delete into ContactoClienteDA

--- Desarrollo/DKCommerceDataAccess/ContactoClienteDA.cs
+++ Desarrollo/DKCommerceDataAccess/ContactoClienteDA.cs
@@ -144,5 +144,37 @@ namespace DKCommerceDataAccess
             }
         }
 
+        public void Delete(int idContactoCliente)
+        {
+            var conn = Configuration.GetConnectionString("DK Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = UpContactoClienteDelete;
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@ContactoId", SqlDbType.Int).Value = idContactoCliente;
+
+                            sqlCmd.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception)
+                        {
+                            sqlTran?.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
