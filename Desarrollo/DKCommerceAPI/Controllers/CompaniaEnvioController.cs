f9cdedecf0dff30c75ad2d9bdc3c0429d94c2d81
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-27 01:28:34 -0500

    Add CompaniaEnvioController into API

---
+++ Desarrollo/DKCommerceAPI/Controllers/CompaniaEnvioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceAPI.Controllers
+{
+    public class CompaniaEnvioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
