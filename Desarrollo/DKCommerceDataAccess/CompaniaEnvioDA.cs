da0fbeebfc2bdf82742ca8e5f97873ed30cb7fef
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-27 00:52:27 -0500

    Add funtion SelectById into CompaniaEnvioDA

--- Desarrollo/DKCommerceDataAccess/CompaniaEnvioDA.cs
+++ Desarrollo/DKCommerceDataAccess/CompaniaEnvioDA.cs
@@ -1,9 +1,8 @@
-﻿using Libreria;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DKCommerceBussinesEntity;
+using Libreria;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DKCommerceDataAccess
 {
@@ -14,5 +13,50 @@ namespace DKCommerceDataAccess
         public const string UpCompaniaEnvioDelete = "UpCompaniaEnvioDelete";
         public const string UpCompaniaEnvioSelById = "UpCompaniaEnvioSelById";
 
+        public CompaniaEnvioBE SelectById(int idCompaniaEnvio)
+        {
+            CompaniaEnvioBE beCompaniaEnvio = null;
+            var conn = Configuration.GetConnectionString("DK Commerce");
+            SqlDataReader dr = null;
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    try
+                    {
+                        sqlCmd.Connection = sqlCon;
+                        sqlCmd.CommandText = UpCompaniaEnvioSelById;
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                        sqlCmd.Parameters.Add("@CompañiaDeEnvioId", SqlDbType.Int).Value = idCompaniaEnvio;
+                        dr = sqlCmd.ExecuteReader();
+
+                        while (dr.Read())
+                        {
+                            beCompaniaEnvio = new CompaniaEnvioBE
+                            {
+                                CompañiaDeEnvioId = idCompaniaEnvio,
+                                NombreCompañia = dr["NombreCompañia"]?.ToString(),
+                                Ruc = dr["Ruc"] == DBNull.Value ? null : Convert.ToString(dr["CargoContacto"])!,
+                                Telefono = dr["Telefono"] == DBNull.Value ? null : Convert.ToString(dr["Telefono"])!
+                            };
+                        }
+
+                        return beCompaniaEnvio;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        dr?.Close();
+                        dr?.Dispose();
+                    }
+                }
+            }
+        }
     }
 }
