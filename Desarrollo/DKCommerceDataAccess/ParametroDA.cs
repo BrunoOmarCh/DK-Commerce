1ae9909982ee02471c39270c95bec914c03de3ab
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-28 20:44:55 -0500

    Añadir Logica SelectById a ParametroDA

--- Desarrollo/DKCommerceDataAccess/ParametroDA.cs
+++ Desarrollo/DKCommerceDataAccess/ParametroDA.cs
@@ -1,17 +1,67 @@
-﻿using System;
+﻿using DKCommerceBussinesEntity;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Libreria;
 
 namespace DKCommerceDataAccess
 {
-    public class ParametroDA
+    public class ParametroDA: ConfigDataAccess
     {
         public const string UpParametroInsert = "UpParametroInsert";
         public const string UpParametroUpdate = "UpParametroUpdate";
         public const string UpParametroDelete = "UpParametroDelete";
         public const string UpParametroSelById = "UpParametroSelById";
-       
+
+        public ParametroBE SelectById(string Claveid)
+        {
+            ParametroBE beParametro= null;
+            var conn = Configuration.GetConnectionString("DK Commerce");
+            SqlDataReader dr = null;
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    try
+                    {
+                        sqlCmd.Connection = sqlCon;
+                        sqlCmd.CommandText = UpParametroInsert;
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                        sqlCmd.Parameters.Add("@Clave", SqlDbType.VarChar).Value = Claveid;
+                        dr = sqlCmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            beParametro = new ParametroBE();
+
+                            beParametro.Clave= Claveid;
+                            beParametro.Grupo= dr["Grupo"] == DBNull.Value ? null : Convert.ToString(dr["Grupo"])!;
+                            beParametro.Valor= dr["Valor"] == DBNull.Value ? null : Convert.ToString(dr["Valor"])!;
+
+                        }
+
+                        return beParametro;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        dr?.Close();
+                        dr?.Dispose();
+                    }
+                }
+            }
+        }
+
+
     }
 }
