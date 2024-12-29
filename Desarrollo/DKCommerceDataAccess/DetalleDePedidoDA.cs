323637b011acb383f293f10b9b3ac7307fbc762d
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-29 15:14:52 -0500

    Añadir Delete into DetalleDePedido

--- Desarrollo/DKCommerceDataAccess/DetalleDePedidoDA.cs
+++ Desarrollo/DKCommerceDataAccess/DetalleDePedidoDA.cs
@@ -147,6 +147,38 @@ namespace DKCommerceDataAccess
                 }
             }
         }
+        public void Delete(int pedidoId)
+        {
+            var conn = Configuration.GetConnectionString("DK Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = "UpDetalleDePedidoDelete"; // Nombre del procedimiento almacenado
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@PedidoId", SqlDbType.Int).Value = pedidoId;
+
+                            sqlCmd.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            sqlTran?.Rollback();
+                            throw ex;
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
