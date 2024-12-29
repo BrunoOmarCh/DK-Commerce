3c6727b659028af2ffcfcc13ccca8a84911c8f97
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-28 20:55:42 -0500

    Añadir Logica Delete a ParametroDA

--- Desarrollo/DKCommerceDataAccess/ParametroDA.cs
+++ Desarrollo/DKCommerceDataAccess/ParametroDA.cs
@@ -133,5 +133,38 @@ namespace DKCommerceDataAccess
                 }
             }
         }
+
+        public void Delete(string idClave)
+        {
+            var conn = Configuration.GetConnectionString("DK Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = UpParametroDelete;
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@Clave", SqlDbType.NVarChar).Value = idClave;
+
+                            sqlCmd.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception)
+                        {
+                            sqlTran?.Rollback();
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
