92f6a3c872440e8e06bffd7dbcef3097fe87321e
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-30 13:54:18 -0500

    Implementacion de capa BL para entidad Parametro

--- Desarrollo/DKCommerceBussinesLogic/ParametroBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ParametroBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,58 @@ namespace DKCommerceBussinesLogic
 {
     public class ParametroBL
     {
+        public ParametroBE SelectById(string idParametro)
+        {
+            try
+            {
+                var daParametro = new ParametroDA();
+                ParametroBE beParametro;
 
+                beParametro = daParametro.SelectById(idParametro);
+                return beParametro;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Insert(ParametroBE beParametro)
+        {
+            try
+            {
+                var daParametro = new ParametroDA();
+                daParametro.Insert(beParametro);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(string idParametro, ParametroBE beParametro)
+        {
+            try
+            {
+                var daParametro= new ParametroDA();
+                daParametro.Update(idParametro, beParametro);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Delete(string idParametro)
+        {
+            try
+            {
+                var daParametro = new ParametroDA();
+                daParametro.Delete(idParametro);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
