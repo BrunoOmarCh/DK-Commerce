59760c4cf3e01224fa6c52e796a8120f6ce3b240
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-30 22:36:42 -0500

    Implementacion de capa BL para entidad Parametro

--- Desarrollo/DKCommerceBussinesLogic/DetalleDePedidoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/DetalleDePedidoBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,59 @@ namespace DKCommerceBussinesLogic
 {
     public class DetalleDePedidoBL
     {
+        public DetalleDePedidoBE SelectById(int idDetalleDePedido)
+        {
+            try
+            {
+                var daDetalleDePedido = new DetalleDePedidoDA();
+                DetalleDePedidoBE beDetalleDePedido;
+
+                beDetalleDePedido = daDetalleDePedido.SelectById(idDetalleDePedido);
+                return beDetalleDePedido;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Insert(DetalleDePedidoBE beDetalleDePedido)
+        {
+            try
+            {
+                var daDetalleDePedido= new DetalleDePedidoDA();
+                daDetalleDePedido.Insert(beDetalleDePedido);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(int idDetalleDePedido, DetalleDePedidoBE beDetalleDePedido)
+        {
+            try
+            {
+                var daDetalleDePedido = new DetalleDePedidoDA();
+                daDetalleDePedido.Update(idDetalleDePedido, beDetalleDePedido);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void Delete(int idDetalleDePedido)
+        {
+            try
+            {
+                var daDetalleDePedidoBE = new DetalleDePedidoDA();
+                daDetalleDePedido.Delete(idDetalleDePedidoBE);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
+}
 }
