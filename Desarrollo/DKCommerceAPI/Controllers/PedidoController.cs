507f5e4f25dbe50aff471a35c809b0f972cc789c
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-01 13:18:56 -0500

    Add SelectById and Insert into PedidoController

--- Desarrollo/DKCommerceAPI/Controllers/PedidoController.cs
+++ Desarrollo/DKCommerceAPI/Controllers/PedidoController.cs
@@ -8,6 +8,22 @@ namespace DKCommerceAPI.Controllers
     [Route("api/pedido")]
     public class PedidoController : ControllerBase
     {
+        [HttpGet]
+        [Route("select-by-id/{idPedido}")]
+        public PedidoBE SelectById(int idPedido)
+        {
+            var blPedido = new PedidoBL();
 
+            return blPedido.SelectById(idPedido);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public void Insert(PedidoBE bePedido)
+        {
+            var blPedido = new PedidoBL();
+
+            blPedido.Insert(bePedido);
+        }
     }
 }
