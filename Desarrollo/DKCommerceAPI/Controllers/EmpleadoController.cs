803ab688356dd3024c9a276839d084baceda52d6
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-31 23:30:14 -0500

    Add SelectById and Insert into ParametroController

--- Desarrollo/DKCommerceAPI/Controllers/EmpleadoController.cs
+++ Desarrollo/DKCommerceAPI/Controllers/EmpleadoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceBussinesLogic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceAPI.Controllers
 {
@@ -6,7 +8,23 @@ namespace DKCommerceAPI.Controllers
     [Route("api/empleado")]
     public class EmpleadoController : ControllerBase
     {
+        [HttpGet]
+        [Route("select-by-id/{idEmpleado}")]
+        public EmpleadoBE SelectById(int idEmpleado)
+        {
+            var blEmpleado = new EmpleadoBL();
 
+            return blEmpleado.SelectById(idEmpleado);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public void Insert(EmpleadoBE beEmpleado)
+        {
+            var blEmpleado = new EmpleadoBL();
+
+            blEmpleado.Insert(beEmpleado);
+        }
 
     }
 }
