f4dfe9918a9650a634649cca47da6d586adbc84a
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-31 23:13:33 -0500

    Add DetallePedidoController into API

---
+++ Desarrollo/DKCommerceAPI/Controllers/DetalleDePedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceAPI.Controllers
+{
+    public class DetalleDePedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
