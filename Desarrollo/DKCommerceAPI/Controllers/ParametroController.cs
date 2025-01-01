130fe97a4673b2186e2c12985ecc127c94c6664b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-31 23:24:26 -0500

    Add SelectById and Insert into route Parametro

--- Desarrollo/DKCommerceAPI/Controllers/ParametroController.cs
+++ Desarrollo/DKCommerceAPI/Controllers/ParametroController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceBussinesLogic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceAPI.Controllers
 {
-    public class ParametroController : Controller
+    [ApiController]
+    [Route("api/parametro")]
+    public class ParametroController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]// Método HTTP GET
+        [Route("select-by-id/{idParametro}")]// Ruta en la web del método
+        public ParametroBE SelectById(string idParametro)
         {
-            return View();
+            var blParametro= new ParametroBL();
+
+            return blParametro.SelectById(idParametro);
         }
+
+        [HttpPost]
+        [Route("insert")]
+        public void Insert(ParametroBE beParametro)
+        {
+            var blParametro = new ParametroBL();
+
+            blParametro.Insert(beParametro);
+        }
+
+
     }
 }
