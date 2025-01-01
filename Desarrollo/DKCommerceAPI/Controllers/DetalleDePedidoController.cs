94f6627f3935d7f7f0c9b18aba61c6ded4bdf34d
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-12-31 23:35:44 -0500

    Add Update and Delete into DetalleDePedidoController

--- Desarrollo/DKCommerceAPI/Controllers/DetalleDePedidoController.cs
+++ Desarrollo/DKCommerceAPI/Controllers/DetalleDePedidoController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DKCommerceBussinesEntity;
+using DKCommerceBussinesLogic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceAPI.Controllers
 {
     [ApiController]
     [Route("api/detalleDePedido")]
 
-    public class DetalleDePedidoController : Controller
+    public class DetalleDePedidoController : ControllerBase
     {
+        [HttpGet]
+        [Route("select-by-id/{idDetalleDePedido}")]
+        public DetalleDePedidoBE SelectById(int idDetalleDePedido)
+        {
+            var blDetalleDePedido = new DetalleDePedidoBL();
+
+            return blDetalleDePedido.SelectById(idDetalleDePedido);
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public void Insert(DetalleDePedidoBE beDetalleDePedido)
+        {
+            var blDetalleDePedido = new DetalleDePedidoBL();
+
+            blDetalleDePedido.Insert(beDetalleDePedido);
+        }
+
 
     }
 }
