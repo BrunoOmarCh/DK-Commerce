f4ed087029385c2dbdb3a1f6c3cc41231a984586
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-02 09:45:37 -0500

    Add DetalleDePedidoController into UI DKCommerce

---
+++ Desarrollo/DKCommerceUI/Controllers/DetalleDePedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceUI.Controllers
+{
+    public class DetalleDePedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
