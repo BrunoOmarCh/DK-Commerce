623bf6beb3849fd3e7fde61efe46e00d8f2e862f
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-02 09:45:59 -0500

    Add CompaniaDeEnvioController into UI DKCommerce

---
+++ Desarrollo/DKCommerceUI/Controllers/CompaniaDeEnvioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKCommerceUI.Controllers
+{
+    public class CompaniaDeEnvioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
