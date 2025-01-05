e6eef78e2dcc013c4125ef394d99aaf45378c879
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:14:51 -0500

    Refactor ProveedorModel into Models

--- Desarrollo/DKCommerceUI/Models/ProveedorModel.cs
+++ Desarrollo/DKCommerceUI/Models/ProveedorModel.cs
@@ -4,17 +4,16 @@
     {
         public int ProveedorId { get; set; }
         public string Nombre { get; set; }
-        public string Ruc { get; set; }
+        public string? Ruc { get; set; }
         public int ContactoId { get; set; }
-        public string Direccion { get; set; }
-        public string Ciudad { get; set; }
-        public string Region { get; set; }
-        public string CodPostal { get; set; }
-        public string Pais { get; set; }
-        public string Telefono { get; set; }
-        public string Fax { get; set; }
-        public string PaginaPrincipal { get; set; }
-
+        public string? Direccion { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Region { get; set; }
+        public string? CodPostal { get; set; }
+        public string? Pais { get; set; }
+        public string? Telefono { get; set; }
+        public string? Fax { get; set; }
+        public string? PaginaPrincipal { get; set; }
 
     }
 }
