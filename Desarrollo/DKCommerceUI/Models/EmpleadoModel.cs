d8a837b4341e1faf9924cef211c64dc8ca9069ff
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:13:36 -0500

    Refactor EmpleadoModel into Models

--- Desarrollo/DKCommerceUI/Models/EmpleadoModel.cs
+++ Desarrollo/DKCommerceUI/Models/EmpleadoModel.cs
@@ -2,5 +2,26 @@
 {
     public class EmpleadoModel
     {
+        public int EmpleadoId { get; set; }
+        public string Nombres { get; set; }
+        public string? ApellidoPaterno { get; set; }
+        public string? ApellidoMaterno { get; set; }
+        public string? TipoDocIdentidad { get; set; }
+        public string? NroDocIdentidad { get; set; }
+        public string? Cargo { get; set; }
+        public string? Tratamiento { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public DateTime? FechaContratación { get; set; }
+        public string? Dirección { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Región { get; set; }
+        public string? CodPostal { get; set; }
+        public string? Pais { get; set; }
+        public string? TelefonoFijo { get; set; }
+        public string? Anexo { get; set; }
+        public string? Notas { get; set; }
+        public int? JefeId { get; set; }
+        public string? Email { get; set; }
+        public string? EstadoCivil { get; set; }
     }
 }
