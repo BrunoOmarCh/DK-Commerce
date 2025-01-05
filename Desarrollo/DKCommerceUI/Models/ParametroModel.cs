b5ede6babc4825130aaca445c1e8b73117c2975b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:14:22 -0500

    Refactor ParametroModel into Models

--- Desarrollo/DKCommerceUI/Models/ParametroModel.cs
+++ Desarrollo/DKCommerceUI/Models/ParametroModel.cs
@@ -2,5 +2,8 @@
 {
     public class ParametroModel
     {
+        public string Clave { get; set; }
+        public string? Grupo { get; set; }
+        public string? Valor { get; set; }
     }
 }
