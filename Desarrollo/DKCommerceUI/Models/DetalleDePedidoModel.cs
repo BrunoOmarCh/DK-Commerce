206fd29c3648e7b6b5300c1644e4790bd37f60a3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:13:54 -0500

    Refactor DetalleDePedidoModel into Models

--- Desarrollo/DKCommerceUI/Models/DetalleDePedidoModel.cs
+++ Desarrollo/DKCommerceUI/Models/DetalleDePedidoModel.cs
@@ -2,5 +2,13 @@
 {
     public class DetalleDePedidoModel
     {
+        public int PedidoId { get; set; }
+        public int ProductoId { get; set; }
+        public decimal PrecioNeto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal? Igv { get; set; }
+        public decimal? Isc { get; set; }
+        public decimal? MontoSubTotal { get; set; }
     }
 }
