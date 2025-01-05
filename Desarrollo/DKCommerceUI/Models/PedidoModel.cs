61e7e6924cc7f02356e380736c6fa137211f9687
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:14:36 -0500

    Refactor PedidoModel into Models

--- Desarrollo/DKCommerceUI/Models/PedidoModel.cs
+++ Desarrollo/DKCommerceUI/Models/PedidoModel.cs
@@ -2,5 +2,21 @@
 {
     public class PedidoModel
     {
+        public int PedidoId { get; set; }
+        public string ClienteId { get; set; }
+        public int? IdEmpleado { get; set; }
+        public DateTime? FechaPedido { get; set; }
+        public DateTime? FechaEntrega { get; set; }
+        public DateTime? FechaEnvío { get; set; }
+        public int? FormaEnvío { get; set; }
+        public decimal? Igv { get; set; }
+        public decimal? Isc { get; set; }
+        public decimal? MontoTotal { get; set; }
+        public string? Destinatario { get; set; }
+        public string? DirecciónDestinatario { get; set; }
+        public string? CiudadDestinatario { get; set; }
+        public string? RegiónDestinatario { get; set; }
+        public string? CódPostalDestinatario { get; set; }
+        public string? PaísDestinatario { get; set; }
     }
 }
