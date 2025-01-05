9091c422ded846b19446ff987b80c4ef2744e9da
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-04 23:06:40 -0500

    Refactor ContactoProveedorModel into Models

--- Desarrollo/DKCommerceUI/Models/ContactoProveedorModel.cs
+++ Desarrollo/DKCommerceUI/Models/ContactoProveedorModel.cs
@@ -2,5 +2,9 @@
 {
     public class ContactoProveedorModel
     {
+        public int ContactoProveedorId { get; set; }
+        public string? NombreContacto { get; set; }
+        public string? CargoContacto { get; set; }
+        public string? Dni { get; set; }
     }
 }
