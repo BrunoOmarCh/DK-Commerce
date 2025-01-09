e6dd2493f4c82995ebdbf06672ca0c84c7881a01
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-08 22:39:13 -0500

    Add constructor and route

--- Desarrollo/DKCommerceUI/Controllers/CompaniaDeEnvioController.cs
+++ Desarrollo/DKCommerceUI/Controllers/CompaniaDeEnvioController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using DKCommerceUI.Models;
+using MercurioUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceUI.Controllers
 {
+    [Route("companiaDeEnvio")]
+
     public class CompaniaDeEnvioController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+            private IMapper _mapper;
+            public CompaniaDeEnvioController()
+            {
+                var config = new MapperConfiguration(
+                    x =>
+                    {
+                        x.AddProfile(new MappingProfile());
+                    }
+                    );
+                _mapper = config.CreateMapper();
+
+            }
+
+  
         }
-    }
 }
