2a425031160285a766e8ca3c9322691ab06bf51f
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-09 21:43:33 -0500

    Add constructor and route

--- Desarrollo/DKCommerceUI/Controllers/EmpleadoController.cs
+++ Desarrollo/DKCommerceUI/Controllers/EmpleadoController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using DKCommerceUI.Models;
+using MercurioUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DKCommerceUI.Controllers
 {
+    [Route("empleado")]
+
     public class EmpleadoController : Controller
     {
-        public IActionResult Index()
+        private IMapper _mapper;
+        public EmpleadoController()
         {
-            return View();
+            var config = new MapperConfiguration(
+                x =>
+                {
+                    x.AddProfile(new MappingProfile());
+                }
+                );
+            _mapper = config.CreateMapper();
+
         }
+
     }
 }
