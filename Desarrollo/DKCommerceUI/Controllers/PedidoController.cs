5c428e10ebf9b94cfd4bc551aa49b26e758eb2e6
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-10 22:51:40 -0500

    Add nuevo and index, select by id and insert

--- Desarrollo/DKCommerceUI/Controllers/PedidoController.cs
+++ Desarrollo/DKCommerceUI/Controllers/PedidoController.cs
@@ -20,6 +20,44 @@ namespace DKCommerceUI.Controllers
             _mapper = config.CreateMapper();
 
         }
- 
+
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("nuevo")]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("select-by-id/{idPedido}")]
+        public async Task<PedidoModel> SelectById(int idPedido)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpPost]
+        [Route("insert")]
+        public async void Insert(string bePedido)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
