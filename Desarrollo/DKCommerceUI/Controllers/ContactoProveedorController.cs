dcd73f5908c595e5761e837e37fb479ef3cb1ded
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-13 23:56:55 -0500

    Add Insert into ContactoProveedorController

--- Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
@@ -84,7 +84,22 @@ namespace DKCommerceUI.Controllers
         {
             try
             {
-            
+                var dtoContactoProveedor = JsonConvert.DeserializeObject<ContactoProveedorModel>(jsonContactoProveedor);
+
+                using (var cliente = new HttpClient())
+                {
+                    cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                    cliente.DefaultRequestHeaders.Clear();
+                    cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var beContactoProveedor = _mapper.Map<ContactoProveedorBE>(dtoContactoProveedor);
+                    var jsonContent = new StringContent(JsonConvert.SerializeObject(beContactoProveedor), Encoding.UTF8, "application/json");
+                    var res = await cliente.PostAsync("api/contactoProveedor/insert", jsonContent);
+                    if (!res.IsSuccessStatusCode)
+                    {
+                        throw new Exception(res.StatusCode.ToString());
+                    }
+                }
+
             }
             catch (Exception ex)
             {
