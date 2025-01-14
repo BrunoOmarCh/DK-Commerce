f13e92051b9d608cdd5c6163386c75e73742c55a
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-13 23:57:09 -0500

    Add Update into ContactoProveedorController

--- Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
@@ -111,7 +111,22 @@ namespace DKCommerceUI.Controllers
         [Route("update/{idContactoProveedor}")]
         public async Task Update(int idContactoProveedor, string  jsonContactoProveedor)
         {
-           
+            var dtoContactoProveedor = JsonConvert.DeserializeObject<ContactoProveedorModel>(jsonContactoProveedor);
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var beContactoProveedor = _mapper.Map<ContactoProveedorBE>(dtoContactoProveedor);
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(beContactoProveedor), Encoding.UTF8, "application/json");
+                var res = await cliente.PutAsync("api/contactoProveedor/update/" + idContactoProveedor, jsonContent);
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
         }
 
         [HttpDelete]
