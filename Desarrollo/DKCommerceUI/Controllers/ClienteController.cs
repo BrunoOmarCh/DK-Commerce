20f6847fa41a2d9bc95e5d12be07426829bc404d
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-01-14 23:47:34 -0500

    Add Delete into ClienteController

--- Desarrollo/DKCommerceUI/Controllers/ClienteController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ClienteController.cs
@@ -131,7 +131,18 @@ namespace DKCommerceUI.Controllers
         [Route("delete/{idCliente}")]
         public async Task Delete(string idCliente)
         {
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                var res = await cliente.DeleteAsync("api/cliente/delete/" + idCliente + "/");
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
         }
     }
 }
