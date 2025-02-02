2c5fd163ff3bfa8686b75cd70a72f5edda12abe1
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-02-01 22:59:18 -0500

    Script Implementador para manejar operaciones CRUD

--- Desarrollo/DKCommerceUI/wwwroot/views/contactoProveedor/editar.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/contactoProveedor/editar.js
@@ -1,2 +1,35 @@
-﻿body {
+﻿"use strict";
+
+function fnUpdate(idContactoProveedor) {
+    let contactoProveedor;
+
+    contactoProveedor = {
+        NombreContacto: $("#NombreContactoTxt").val(),
+        CargoContacto: $("#CargoContactoTxt").val(),
+        Dni: $("#DniTxt").val()
+    };
+
+    $.ajax({
+        type: "PUT",
+        async: true,
+        cache: false,
+        data: { contactoProveedor: JSON.stringify(contactoProveedor) },
+        url: "https://localhost:7220/contactoProveedor/update/" + idContactoProveedor,
+        dataType: "text",
+        crossDomain: true,
+        success: function () {
+            alert("Se actualizó el contacto: " + $("#NombreContactoTxt").val());
+        },
+        error: function (param1, param2, param3) {
+            console.error("param1", param1);
+            console.error("param2", param2);
+            console.error("param3", param3);
+            alert("Ocurrió un error al actualizar");
+        }
+    });
 }
+
+$("#GuardarBtn").on("click", function () {
+    const idContactoProveedor = $("#ContactoProveedorIdTxt").val();
+    fnUpdate(idContactoProveedor);
+});
