0a995331351f04e516f0d00af509a4f4c271a4cb
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-02-01 22:58:53 -0500

    Script Implementador para manejar operaciones CRUD

--- Desarrollo/DKCommerceUI/wwwroot/views/contactoCliente/index.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/contactoCliente/index.js
@@ -1,58 +1,56 @@
 ﻿"use strict"; // Fuerza a declarar variables
 
-function fnEliminar(idProducto) {
+function fnEliminar(idContacto) {
     $.ajax({
         type: "DELETE",
-        contentType: "application/json; chartset=utf-8",// Trabaja con objetos Json; en formato UTF-8 (soporta tildes)
+        contentType: "application/json; charset=utf-8", // Trabaja con objetos Json; en formato UTF-8 (soporta tildes)
         async: true,
         cache: false,
-        url: "https://localhost:7220/producto/delete/" + idProducto,
+        url: "https://localhost:7220/contactocliente/delete/" + idContacto,
         success: function () {
-            alert("Se eliminó el producto de código '" + idProducto + "', con éxito.");
+            alert("Se eliminó el contacto de código '" + idContacto + "', con éxito.");
         },
         error: function (param1, param2, param3) {
             console.error("param1", param1);
             console.error("param2", param2);
             console.error("param3", param3);
-            alert("No se pudo eliminar el producto de código '" + idProducto + "'.");
+            alert("No se pudo eliminar el contacto de código '" + idContacto + "'.");
         }
     });
 }
 
-function fnGet(idProducto) {
+function fnGet(idContacto) {
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         async: true,
         cache: false,
-        url: "https://localhost:7220/producto/select-by-id/" + idProducto,
+        url: "https://localhost:7220/contactocliente/select-by-id/" + idContacto,
         success: function (data) {
             console.info("data:", data);
-            var producto = "Nombre: " + data.NombreProducto + "<br>" +
-                "Precio lista: " + data.PrecioLista + "<br>" +
-                "Descuento: " + data.Descuento + "<br>" +
-                "Precio venta: " + data.PrecioVenta;
+            var contacto = "Nombre Contacto: " + data.NombreContacto + "<br>" +
+                "Cargo: " + data.CargoContacto + "<br>" +
+                "Tipo Documento: " + data.TipoDocumento + "<br>" +
+                "Número Documento: " + data.NroDocumento;
 
-            $("#ProductoResult").html(producto);
+            $("#ContactoClienteResult").html(contacto);
         },
         error: function (param1, param2, param3) {
             console.error("param1", param1);
             console.error("param2", param2);
             console.error("param3", param3);
-            alert("No se pudo leer el producto de código '" + idProducto + "'.");
+            alert("No se pudo leer el contacto de código '" + idContacto + "'.");
         }
     });
 }
 
 $("#BuscarBtn").on("click", function () {
-    let idProdBuscar = $("#ProductoBuscarTxt").val();
-
-    fnGet(idProdBuscar);
+    let idContactoBuscar = $("#ContactoClienteBuscarTxt").val();
+    fnGet(idContactoBuscar);
 });
 
 $("#EliminarBtn").on("click", function () {
-    let idProd = $("#IdProductoTxt").val();
-
-    fnEliminar(idProd);
+    let idContacto = $("#ContactoIdTxt").val();
+    fnEliminar(idContacto);
 });
