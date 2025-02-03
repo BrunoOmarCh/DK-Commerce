03b82ccf57fb8d54d51052f6e051e76d208ca9bb
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-02-03 14:20:50 -0500

    Add logic index.js

--- Desarrollo/DKCommerceUI/wwwroot/views/parametro/index.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/parametro/index.js
@@ -1,63 +1,65 @@
 ﻿"use strict"; // Fuerza a declarar variables correctamente
 
-function fnEliminarCategoria(idCategoria) {
+function fnEliminar(idCliente) {
     $.ajax({
         type: "DELETE",
-        contentType: "application/json; charset=utf-8", // Trabaja con objetos JSON en formato UTF-8
+        contentType: "application/json; charset=utf-8", // Soporta caracteres especiales
         async: true,
         cache: false,
-        url: "https://localhost:7220/categoria/delete/" + idCategoria,
+        url: "https://localhost:7220/cliente/delete/" + idCliente,
         success: function () {
-            alert("Se eliminó la categoría de código '" + idCategoria + "' con éxito.");
+            alert("Se eliminó el cliente con ID '" + idCliente + "' correctamente.");
         },
         error: function (param1, param2, param3) {
-            console.error("Error Param1:", param1);
-            console.error("Error Param2:", param2);
-            console.error("Error Param3:", param3);
-            alert("No se pudo eliminar la categoría de código '" + idCategoria + "'.");
+            console.error("param1", param1);
+            console.error("param2", param2);
+            console.error("param3", param3);
+            alert("No se pudo eliminar el cliente con ID '" + idCliente + "'.");
         }
     });
 }
 
-function fnGetCategoria(idCategoria) {
+function fnGet(idCliente) {
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         async: true,
         cache: false,
-        url: "https://localhost:7220/categoria/select-by-id/" + idCategoria,
+        url: "https://localhost:7220/cliente/select-by-id/" + idCliente,
         success: function (data) {
-            console.info("Datos recibidos:", data);
-            const categoria = "Nombre: " + data.Nombre + "<br>" +
-                "Descripción: " + (data.Descripcion || "N/A") + "<br>" +
-                "Suspendido: " + (data.Suspendido ? "Sí" : "No");
+            console.info("data:", data);
+            var cliente = "Nombre/Razón Social: " + data.NombreRazonSocial + "<br>" +
+                "Tipo de Documento: " + (data.TipoDocumento || "No registrado") + "<br>" +
+                "Número de Documento: " + (data.NroDocumento || "No registrado") + "<br>" +
+                "Contacto ID: " + (data.ContactoId || "No registrado") + "<br>" +
+                "Dirección: " + (data.Direccion || "No registrada") + "<br>" +
+                "Ciudad: " + (data.Ciudad || "No registrada") + "<br>" +
+                "Región: " + (data.Region || "No registrada") + "<br>" +
+                "Código Postal: " + (data.CodPostal || "No registrado") + "<br>" +
+                "País: " + (data.Pais || "No registrado") + "<br>" +
+                "Teléfono: " + (data.Telefono || "No registrado") + "<br>" +
+                "Fax: " + (data.Fax || "No registrado");
 
-            $("#CategoriaResult").html(categoria);
+            $("#ClienteResult").html(cliente);
         },
         error: function (param1, param2, param3) {
-            console.error("Error Param1:", param1);
-            console.error("Error Param2:", param2);
-            console.error("Error Param3:", param3);
-            alert("No se pudo obtener la categoría de código '" + idCategoria + "'.");
+            console.error("param1", param1);
+            console.error("param2", param2);
+            console.error("param3", param3);
+            alert("No se pudo leer el cliente con ID '" + idCliente + "'.");
         }
     });
 }
 
 $("#BuscarBtn").on("click", function () {
-    const idCategoriaBuscar = $("#CategoriaBuscarTxt").val();
-    if (!idCategoriaBuscar) {
-        alert("Por favor, ingrese un código de categoría para buscar.");
-        return;
-    }
-    fnGetCategoria(idCategoriaBuscar);
+    let idClienteBuscar = $("#ClienteBuscarTxt").val();
+
+    fnGet(idClienteBuscar);
 });
 
 $("#EliminarBtn").on("click", function () {
-    const idCategoria = $("#CategoriaIdTxt").val();
-    if (!idCategoria) {
-        alert("Por favor, ingrese un código de categoría para eliminar.");
-        return;
-    }
-    fnEliminarCategoria(idCategoria);
+    let idCliente = $("#ClienteIdTxt").val();
+
+    fnEliminar(idCliente);
 });
