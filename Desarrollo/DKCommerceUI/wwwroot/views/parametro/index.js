2655ce3b366a6d194ba283c6576ebbc64291a09c
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-02-04 14:48:30 -0500

    Add logic index.js

--- Desarrollo/DKCommerceUI/wwwroot/views/parametro/index.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/parametro/index.js
@@ -1,65 +1,57 @@
-﻿"use strict"; // Fuerza a declarar variables correctamente
+﻿"use strict"; // Forzar declaración de variables correctamente
 
-function fnEliminar(idCliente) {
+function fnEliminar(idCompania) {
     $.ajax({
         type: "DELETE",
         contentType: "application/json; charset=utf-8", // Soporta caracteres especiales
         async: true,
         cache: false,
-        url: "https://localhost:7220/cliente/delete/" + idCliente,
+        url: "https://localhost:7220/companiaDeEnvio/delete/" + idCompania,
         success: function () {
-            alert("Se eliminó el cliente con ID '" + idCliente + "' correctamente.");
+            alert("Se eliminó la compañía de envío con ID '" + idCompania + "' correctamente.");
         },
         error: function (param1, param2, param3) {
             console.error("param1", param1);
             console.error("param2", param2);
             console.error("param3", param3);
-            alert("No se pudo eliminar el cliente con ID '" + idCliente + "'.");
+            alert("No se pudo eliminar la compañía de envío con ID '" + idCompania + "'.");
         }
     });
 }
 
-function fnGet(idCliente) {
+function fnGet(idCompania) {
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         async: true,
         cache: false,
-        url: "https://localhost:7220/cliente/select-by-id/" + idCliente,
+        url: "https://localhost:7220/companiaDeEnvio/select-by-id/" + idCompania,
         success: function (data) {
             console.info("data:", data);
-            var cliente = "Nombre/Razón Social: " + data.NombreRazonSocial + "<br>" +
-                "Tipo de Documento: " + (data.TipoDocumento || "No registrado") + "<br>" +
-                "Número de Documento: " + (data.NroDocumento || "No registrado") + "<br>" +
-                "Contacto ID: " + (data.ContactoId || "No registrado") + "<br>" +
-                "Dirección: " + (data.Direccion || "No registrada") + "<br>" +
-                "Ciudad: " + (data.Ciudad || "No registrada") + "<br>" +
-                "Región: " + (data.Region || "No registrada") + "<br>" +
-                "Código Postal: " + (data.CodPostal || "No registrado") + "<br>" +
-                "País: " + (data.Pais || "No registrado") + "<br>" +
-                "Teléfono: " + (data.Telefono || "No registrado") + "<br>" +
-                "Fax: " + (data.Fax || "No registrado");
+            var compania = "Nombre de la Compañía: " + data.NombreCompañia + "<br>" +
+                "RUC: " + (data.Ruc || "No registrado") + "<br>" +
+                "Teléfono: " + (data.Telefono || "No registrado");
 
-            $("#ClienteResult").html(cliente);
+            $("#CompaniaDeEnvioResult").html(compania);
         },
         error: function (param1, param2, param3) {
             console.error("param1", param1);
             console.error("param2", param2);
             console.error("param3", param3);
-            alert("No se pudo leer el cliente con ID '" + idCliente + "'.");
+            alert("No se pudo leer la compañía de envío con ID '" + idCompania + "'.");
         }
     });
 }
 
 $("#BuscarBtn").on("click", function () {
-    let idClienteBuscar = $("#ClienteBuscarTxt").val();
+    let idCompaniaBuscar = $("#CompaniaDeEnvioBuscarTxt").val();
 
-    fnGet(idClienteBuscar);
+    fnGet(idCompaniaBuscar);
 });
 
 $("#EliminarBtn").on("click", function () {
-    let idCliente = $("#ClienteIdTxt").val();
+    let idCompania = $("#CompaniaDeEnvioIdTxt").val();
 
-    fnEliminar(idCliente);
+    fnEliminar(idCompania);
 });
