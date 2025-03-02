baa74d54f505322c8277a3dffc7a0b4f6f542d9b
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-01 20:30:42 -0500

    Add Method Delete into ParametroController

--- Desarrollo/DKCommerceUI/Controllers/ParametroController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ParametroController.cs
@@ -148,7 +148,22 @@ namespace DKCommerceUI.Controllers
                 }
             }
         }
+        [HttpDelete]
+        [Route("delete/{stringParametro}")]
+        public async Task Delete(string stringParametro)
+        {
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-
+                var res = await cliente.DeleteAsync("api/parametro/delete/" + stringParametro + "/");
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
     }
 }
