dbbed0124884775e165fc647b19b76195eadfcf3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-03 17:40:03 -0500

    Add Method Delete into DetalleDePedidoController

--- Desarrollo/DKCommerceUI/Controllers/DetalleDePedidoController.cs
+++ Desarrollo/DKCommerceUI/Controllers/DetalleDePedidoController.cs
@@ -151,6 +151,22 @@ namespace DKCommerceUI.Controllers
                 }
             }
         }
+        [HttpDelete]
+        [Route("delete/{idDetalleDePedido}")]
+        public async Task Delete(int idDetalleDePedido)
+        {
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                var res = await cliente.DeleteAsync("api/detalleDePedido/delete/" + idDetalleDePedido + "/");
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
     }
 }
