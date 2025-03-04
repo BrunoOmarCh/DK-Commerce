33df3456e61c92ad45f8ac4eab2283b141f2bc0c
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-04 10:51:52 -0500

    Add Method Deleteinto ContactoProveedorController

--- Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
@@ -150,6 +150,22 @@ namespace DKCommerceUI.Controllers
                 }
             }
         }
+        [HttpDelete]
+        [Route("delete/{idContactoProveedor}")]
+        public async Task Delete(int idContactoProveedor)
+        {
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                var res = await cliente.DeleteAsync("api/contactoProveedor/delete/" + idContactoProveedor + "/");
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
     }
 }
