be83df7ff4318061b4b468f2570db888e9789907
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-04 10:51:36 -0500

    Add Method Update into ContactoProveedorController

--- Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
+++ Desarrollo/DKCommerceUI/Controllers/ContactoProveedorController.cs
@@ -129,6 +129,27 @@ namespace DKCommerceUI.Controllers
                 throw ex;
             }
         }
+        [HttpPut]
+        [Route("update/{idContactoProveedor}")]
+        public async Task Update(int idContactoProveedor, string jsonContactoProveedor)
+        {
+            var dtoContactoProveedor = JsonConvert.DeserializeObject<ContactoProveedorModel>(jsonContactoProveedor);
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var beContactoProveedor = _mapper.Map<ContactoProveedorBE>(dtoContactoProveedor);
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(beContactoProveedor), Encoding.UTF8, "application/json");
+                var res = await cliente.PutAsync("api/contactoProveedor/update/" + idContactoProveedor, jsonContent);
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
 
     }
 }
