86a2de8f4f43e4ff44f990744d56a7ab66a98335
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-06 11:55:15 -0500

    Remove Error

--- Desarrollo/DKCommerceUI/Controllers/CategoriaController.cs
+++ Desarrollo/DKCommerceUI/Controllers/CategoriaController.cs
@@ -26,5 +26,145 @@ namespace DKCommerceUI.Controllers
 
         }
 
+
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("nuevo")]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("editar/{idCategoria}")]
+        public async Task<IActionResult> Editar(int idCategoria)
+        {
+            var dtoCategoria = new CategoriaModel();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res = await client.GetAsync("api/categoria/select-by-id/" + idCategoria + "/");
+                if (res.IsSuccessStatusCode)
+                {
+                    var mainResult = await res.Content.ReadAsStringAsync();
+                    var beCategoria = JsonConvert.DeserializeObject<CategoriaBE>(mainResult);
+
+                    dtoCategoria = _mapper.Map<CategoriaModel>(beCategoria);
+                }
+            }
+
+            ViewBag.CategoriaId = dtoCategoria.CategoriaId;
+            ViewBag.Nombre = dtoCategoria.Nombre;
+            ViewBag.Descripcion = dtoCategoria.Descripcion;
+            ViewBag.Suspendido = dtoCategoria.Suspendido;
+
+            return View(dtoCategoria);
+        }
+
+
+
+        [HttpGet]
+        [Route("select-by-id/{idCategoria}")]
+        public async Task<CategoriaModel> SelectById(int idCategoria)
+        {
+            try
+            {
+                CategoriaModel dtoCategoria = null;
+                CategoriaBE beCategoria = null;
+
+                using (var cliente = new HttpClient())
+                {
+                    cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                    cliente.DefaultRequestHeaders.Clear();
+                    cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var res = await cliente.GetAsync("api/categoria/select-by-id/" + idCategoria + "/");
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var categoriaResult = res.Content.ReadAsStringAsync().Result;
+                        beCategoria = JsonConvert.DeserializeObject<CategoriaBE>(categoriaResult)!;
+                        dtoCategoria = _mapper.Map<CategoriaModel>(beCategoria);
+                    }
+                }
+                return dtoCategoria;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpPost]
+        [Route("insert")]
+        public async void Insert(string jsonCategoria)// Javascript esta enviando un string por un método Ajax
+        {
+            try
+            {
+                var dtoCategoria = JsonConvert.DeserializeObject<CategoriaModel>(jsonCategoria);
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceApi"));
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var beCategoria = _mapper.Map<CategoriaBE>(dtoCategoria);
+                    var content = new StringContent(JsonConvert.SerializeObject(beCategoria), Encoding.UTF8, "application/json");
+                    var res = await client.PostAsync("api/categoria/insert", content);
+                    if (!res.IsSuccessStatusCode)
+                    {
+                        throw new Exception(res.StatusCode.ToString());
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpPut]
+        [Route("update/{idCategoria}")]
+        public async Task Update(int idCategoria, string jsonCategoria)
+        {
+            var dtoCategoria = JsonConvert.DeserializeObject<CategoriaModel>(jsonCategoria);
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKComerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var beCategoria = _mapper.Map<CategoriaBE>(dtoCategoria);
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(beCategoria), Encoding.UTF8, "application/json");
+                var res = await cliente.PutAsync("api/categoria/update/" + idCategoria, jsonContent);
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
+        [HttpDelete]
+        [Route("delete/{idCategoria}")]
+        public async Task Delete(int idCategoria)
+        {
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(ConfigurationJson.GetAppSettings("DKCommerceAPI"));
+                cliente.DefaultRequestHeaders.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res = await cliente.DeleteAsync("api/categoria/delete/" + idCategoria + "/");
+                if (!res.IsSuccessStatusCode)
+                {
+                    throw new Exception(res.StatusCode.ToString());
+                }
+            }
+        }
     }
 }
