02a0da1fe2491679379afad87d037995dd1c070e
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-06 11:45:59 -0500

    Add view new and index

--- Desarrollo/DKCommerceUI/Controllers/CompaniaDeEnvioController.cs
+++ Desarrollo/DKCommerceUI/Controllers/CompaniaDeEnvioController.cs
@@ -27,5 +27,19 @@ namespace DKCommerceUI.Controllers
 
         }
 
+
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("nuevo")]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+
     }
 }
