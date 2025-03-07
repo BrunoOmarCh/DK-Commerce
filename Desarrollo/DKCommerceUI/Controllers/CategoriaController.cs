cd89eabc80f77855d98e7ffa4f6b9802f87815a8
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-07 11:56:43 -0500

    Add view new and index

--- Desarrollo/DKCommerceUI/Controllers/CategoriaController.cs
+++ Desarrollo/DKCommerceUI/Controllers/CategoriaController.cs
@@ -25,6 +25,18 @@ namespace DKCommerceUI.Controllers
             _mapper = config.CreateMapper();
 
         }
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("nuevo")]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
 
     }
 }
