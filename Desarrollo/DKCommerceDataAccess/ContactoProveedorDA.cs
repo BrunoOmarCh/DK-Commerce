9c2eaeb9737b9350f034904c14ee85c080c09be8
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-08 19:53:09 -0500

    Add Method SelectById into DA for ContactoProveedor

--- Desarrollo/DKCommerceDataAccess/ContactoProveedorDA.cs
+++ Desarrollo/DKCommerceDataAccess/ContactoProveedorDA.cs
@@ -19,6 +19,50 @@ namespace DKCommerceDataAccess
         public const string UpContactoProveedorSelById = "UpContactoProveedorSelById";
 
 
+        public ContactoProveedorBE SelectById(int ContactoProveedorId)
+        {
+            ContactoProveedorBE beContactoProveedor = null;
+            var conn = Configuration.GetConnectionString("DK Commerce");
+            SqlDataReader dr = null;
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    try
+                    {
+                        sqlCmd.Connection = sqlCon;
+                        sqlCmd.CommandText = UpContactoProveedorSelById;
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                        sqlCmd.Parameters.Add("@ContactoProveedorId", SqlDbType.Int).Value = ContactoProveedorId;
+                        dr = sqlCmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            beContactoProveedor = new ContactoProveedorBE();
+
+                            beContactoProveedor.ContactoProveedorId = ContactoProveedorId;
+                            beContactoProveedor.NombreContacto = dr["NombreContacto"] == DBNull.Value ? null : Convert.ToString(dr["NombreContacto"])!;
+                            beContactoProveedor.CargoContacto = dr["CargoContacto"] == DBNull.Value ? null : Convert.ToString(dr["CargoContacto"])!;
+                            beContactoProveedor.Dni = dr["Dni"] == DBNull.Value ? null : Convert.ToString(dr["Dni"])!;
+
+                        }
+                        return beContactoProveedor;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        dr?.Close();
+                        dr?.Dispose();
+                    }
+                }
+            }
+        }
+
 
     }
 }
