5b41b1d1883da9f5ed056adcfba4932b4fa6a363
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-09 17:20:08 -0500

    Add Method Insert into DA for DetalleDePedidoDA

--- Desarrollo/DKCommerceDataAccess/DetalleDePedidoDA.cs
+++ Desarrollo/DKCommerceDataAccess/DetalleDePedidoDA.cs
@@ -68,6 +68,45 @@ namespace DKCommerceDataAccess
                 }
             }
         }
+        public void Insert(DetalleDePedidoBE detallebe)
+        {
+            var conn = Configuration.GetConnectionString("DK Commerce");
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    using (var sqlTran = sqlCon.BeginTransaction())
+                    {
+                        try
+                        {
+                            sqlCmd.Connection = sqlCon;
+                            sqlCmd.CommandText = "UpDetalleDePedidoInsert"; 
+                            sqlCmd.CommandType = CommandType.StoredProcedure;
+                            sqlCmd.Transaction = sqlTran;
+
+                            sqlCmd.Parameters.Add("@PedidoId", SqlDbType.Int).Value = detallebe.PedidoId;
+                            sqlCmd.Parameters.Add("@ProductoId", SqlDbType.Int).Value = detallebe.ProductoId;
+                            sqlCmd.Parameters.Add("@PrecioNeto", SqlDbType.Decimal).Value = detallebe.PrecioNeto;
+                            sqlCmd.Parameters.Add("@Cantidad", SqlDbType.Int).Value = detallebe.Cantidad;
+                            sqlCmd.Parameters.Add("@Descuento", SqlDbType.Decimal).Value = detallebe.Descuento;
+                            sqlCmd.Parameters.Add("@Igv", SqlDbType.Decimal).Value = detallebe.Igv ?? (object)DBNull.Value;
+                            sqlCmd.Parameters.Add("@Isc", SqlDbType.Decimal).Value = detallebe.Isc ?? (object)DBNull.Value;
+                            sqlCmd.Parameters.Add("@MontoSubTotal", SqlDbType.Decimal).Value = detallebe.MontoSubTotal ?? (object)DBNull.Value;
+
+                            sqlCmd.ExecuteNonQuery();
+                            sqlTran.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            sqlTran?.Rollback();
+                            throw ex;
+                        }
+                    }
+                }
+            }
+        }
 
 
 
