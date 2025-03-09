eab71872097b28d8193855cfc2ebe86add918510
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-08 19:56:21 -0500

    Add Method SelectById into DA for ContactoCliente

--- Desarrollo/DKCommerceDataAccess/ContactoClienteDA.cs
+++ Desarrollo/DKCommerceDataAccess/ContactoClienteDA.cs
@@ -18,6 +18,52 @@ namespace DKCommerceDataAccess
         public const string UpContactoClienteDelete = "UpContactoClienteDelete";
         public const string UpContactoClienteSelById = "UpContactoClienteSelById";
 
- 
+
+        public ContactoClienteBE SelectById(int ContactoClienteId)
+        {
+            ContactoClienteBE beContactoCliente = null;
+            var conn = Configuration.GetConnectionString("DK Commerce");
+            SqlDataReader dr = null;
+
+            using (var sqlCon = new SqlConnection(conn))
+            {
+                sqlCon.Open();
+                using (var sqlCmd = new SqlCommand())
+                {
+                    try
+                    {
+                        sqlCmd.Connection = sqlCon;
+                        sqlCmd.CommandText = UpContactoClienteInsert;
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                        sqlCmd.Parameters.Add("@ContactoId", SqlDbType.Int).Value = ContactoClienteId;
+                        dr = sqlCmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            beContactoCliente = new ContactoClienteBE();
+
+                            beContactoCliente.ContactoId = ContactoClienteId;
+                            beContactoCliente.NombreContacto = dr["NombreContacto"] == DBNull.Value ? null : Convert.ToString(dr["NombreContacto"])!;
+                            beContactoCliente.CargoContacto = dr["CargoContacto"] == DBNull.Value ? null : Convert.ToString(dr["CargoContacto"])!;
+                            beContactoCliente.TipoDocumento = dr["TipoDocumento"] == DBNull.Value ? null : Convert.ToString(dr["TipoDocumento"])!;
+                            beContactoCliente.NroDocumento = dr["NroDocumento"] == DBNull.Value ? null : Convert.ToString(dr["NroDocumento"])!;
+
+                        }
+
+                        return beContactoCliente;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        dr?.Close();
+                        dr?.Dispose();
+                    }
+                }
+            }
+        }
+
     }
 }
