830b478805bf3ca0acee1c163bebb2a011a6e7e8
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-10 21:15:00 -0500

    Agregar metodo para obtener cliente por ID

--- Desarrollo/DKCommerceBussinesLogic/ClienteBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ClienteBL.cs
@@ -10,6 +10,21 @@ namespace DKCommerceBussinesLogic
 {
     public class ClienteBL
     {
+        public ClienteBE SelectById(string clienteId)
+        {
+            try
+            {
+                var daCliente = new ClienteDA();
+                ClienteBE beCliente;
+
+                beCliente = daCliente.SelectById(clienteId);
+                return beCliente;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
     }
 }
