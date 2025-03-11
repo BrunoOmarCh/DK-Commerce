86c62415fcb90b6bdf1a41c187b4bbf827b996f3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-10 21:07:44 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/CategoriaBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/CategoriaBL.cs
@@ -10,68 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class CategoriaBL
     {
-        public CategoriaBE SelectById(int idCategoria)
-        {
-            // var daCategoria = new CategoriaDA();
-            // return daCategoria.SelectById(idCategoria);
-            try
-            {
-                var daCategoria = new CategoriaDA();
-                CategoriaBE beCategoria;
 
-                beCategoria = daCategoria.SelectById(idCategoria);
-                return beCategoria;
-            }
-            catch (Exception ex)
-            {
-                // Se habilita un log de eventos
-
-                throw ex;
-            } 
-        }
-
-        public void Insert(CategoriaBE beCategoria)
-        {
-            // var daCategoria = new CategoriaDA();
-            // daCategoria.Insert(beCategoria);
-            try
-            {
-
-                var daCategoria = new CategoriaDA();
-
-                daCategoria.Insert(beCategoria);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Update(int idCategoria, CategoriaBE beCategoria)
-        {
-            try
-            {
-                var daCategoria = new CategoriaDA();
-
-                daCategoria.Update(idCategoria, beCategoria);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Delete(int idCategoria)
-        {
-            try
-            {
-                var daCategoria = new CategoriaDA();
-
-                daCategoria.Delete(idCategoria);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        
     }
 }
