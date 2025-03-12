72b49380f5154cfe5d40ac064ad530c4b0e0d4a3
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-12 15:23:46 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/ContactoProveedorBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ContactoProveedorBL.cs
@@ -10,61 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class ContactoProveedorBL
     {
-        public ContactoProveedorBE SelectById(int idContactoProveedor)
-        {
-            try
-            {
-                var daContactoProveedor = new ContactoProveedorDA();
-                ContactoProveedorBE beContactoProveedor;
 
-                beContactoProveedor = daContactoProveedor.SelectById(idContactoProveedor);
-                return beContactoProveedor;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Insert(ContactoProveedorBE beContactoProveedor)
-        {
-            try
-            {
-                var daContactoProveedor = new ContactoProveedorDA();
-                daContactoProveedor.Insert(beContactoProveedor);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Update(int idContactoProveedor, ContactoProveedorBE beContactoProveedor)
-        {
-            try
-            {
-                var daContactoProveedor = new ContactoProveedorDA();
-
-                daContactoProveedor.Update(idContactoProveedor, beContactoProveedor);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Delete(int idContactoProveedor)
-        {
-            try
-            {
-                var daContactoProveedor= new ContactoProveedorDA();
-
-                daContactoProveedor.Delete(idContactoProveedor);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
