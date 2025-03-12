cbbdfa323e7660f4ebaab9c80f11e97ef97511fe
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-12 15:29:20 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/DetalleDePedidoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/DetalleDePedidoBL.cs
@@ -10,59 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class DetalleDePedidoBL
     {
-        public DetalleDePedidoBE SelectById(int idDetalleDePedido, int productoId)
-        {
-            try
-            {
-                var daDetalleDePedido = new DetalleDePedidoDA();
-                DetalleDePedidoBE beDetalleDePedido;
-
-                beDetalleDePedido = daDetalleDePedido.SelectById(idDetalleDePedido,  productoId);
-                return beDetalleDePedido;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Insert(DetalleDePedidoBE beDetalleDePedido)
-        {
-            try
-            {
-                var daDetalleDePedido= new DetalleDePedidoDA();
-                daDetalleDePedido.Insert(beDetalleDePedido);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Update(int idDetalleDePedido, int productoId, DetalleDePedidoBE beDetalleDePedido)
-        {
-            try
-            {
-                var daDetalleDePedido = new DetalleDePedidoDA();
-                daDetalleDePedido.Update(idDetalleDePedido, productoId, beDetalleDePedido);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Delete(int idDetalleDePedido, int productoId)
-        {
-            try
-            {
-                var daDetalleDePedido = new DetalleDePedidoDA();
-                daDetalleDePedido.Delete(idDetalleDePedido, productoId);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
 
