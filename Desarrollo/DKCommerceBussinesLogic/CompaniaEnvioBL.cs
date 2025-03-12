0476ba699a16ac50758301157588aa0bf6b4b352
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-11 21:18:08 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/CompaniaEnvioBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/CompaniaEnvioBL.cs
@@ -10,58 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class CompaniaEnvioBL
     {
-        public CompaniaEnvioBE SelectById(int idCompaniaEnvio)
-        {
-            try
-            {
-                var daCompaniaEnvio = new CompaniaEnvioDA();
-                CompaniaEnvioBE beCompaniaEnvio;
-
-                beCompaniaEnvio= daCompaniaEnvio.SelectById(idCompaniaEnvio);
-                return beCompaniaEnvio;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Insert(CompaniaEnvioBE beCompaniaEnvio)
-        {
-            try
-            {
-                var daCompaniaEnvio = new CompaniaEnvioDA();
-                daCompaniaEnvio.Insert(beCompaniaEnvio);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Update(int idCompaniaEnvio, CompaniaEnvioBE beCompaniaEnvio)
-        {
-            try
-            {
-                var daCompaniaEnvio = new CompaniaEnvioDA();
-                daCompaniaEnvio.Update(idCompaniaEnvio, beCompaniaEnvio);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Delete(int idCompaniaEnvio)
-        {
-            try
-            {
-                var daCompaniaEnvio = new CompaniaEnvioDA();
-                daCompaniaEnvio.Delete(idCompaniaEnvio);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
+  
     }
 }
