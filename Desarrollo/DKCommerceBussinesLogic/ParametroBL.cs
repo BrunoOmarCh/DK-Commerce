c0fd9641996cab7725f54269c86b5ee91cb8bd40
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-13 17:57:15 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/ParametroBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ParametroBL.cs
@@ -10,58 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class ParametroBL
     {
-        public ParametroBE SelectById(string idParametro)
-        {
-            try
-            {
-                var daParametro = new ParametroDA();
-                ParametroBE beParametro;
 
-                beParametro = daParametro.SelectById(idParametro);
-                return beParametro;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Insert(ParametroBE beParametro)
-        {
-            try
-            {
-                var daParametro = new ParametroDA();
-                daParametro.Insert(beParametro);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Update(string idParametro, ParametroBE beParametro)
-        {
-            try
-            {
-                var daParametro= new ParametroDA();
-                daParametro.Update(idParametro, beParametro);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Delete(string idParametro)
-        {
-            try
-            {
-                var daParametro = new ParametroDA();
-                daParametro.Delete(idParametro);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
