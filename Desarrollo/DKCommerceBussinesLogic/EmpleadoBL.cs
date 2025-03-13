4effeaf1ecc147dd495d98c0a812b2be032b0de5
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-13 17:54:58 -0500

    Refactor reoute

--- Desarrollo/DKCommerceBussinesLogic/EmpleadoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/EmpleadoBL.cs
@@ -10,59 +10,6 @@ namespace DKCommerceBussinesLogic
 {
     public class EmpleadoBL
     {
-        public EmpleadoBE SelectById(int idEmpleado)
-        {
-            try
-            {
-                var daEmpleado= new EmpleadoDA();
-                EmpleadoBE beEmpleado;
-
-                beEmpleado = daEmpleado.SelectById(idEmpleado);
-                return beEmpleado;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Insert(EmpleadoBE beEmpleado)
-        {
-            try
-            {
-                var daEmpleado = new EmpleadoDA();
-                daEmpleado.Insert(beEmpleado);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public void Update(int idEmpleado, EmpleadoBE beEmpleado)
-        {
-            try
-            {
-                var daEmpleado = new EmpleadoDA();
-                daEmpleado.Update(idEmpleado, beEmpleado);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-        public void Delete(int idEmpleado)
-        {
-            try
-            {
-                var daEmpleado = new EmpleadoDA();
-                daEmpleado.Delete(idEmpleado);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
 
     }
 }
