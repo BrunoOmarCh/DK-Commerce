7afe811a1019950642a6d6cff67eeac1492271d4
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-13 17:55:19 -0500

    Agregar metodo para obtener Empleado por ID

--- Desarrollo/DKCommerceBussinesLogic/EmpleadoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/EmpleadoBL.cs
@@ -10,6 +10,21 @@ namespace DKCommerceBussinesLogic
 {
     public class EmpleadoBL
     {
+        public EmpleadoBE SelectById(int idEmpleado)
+        {
+            try
+            {
+                var daEmpleado = new EmpleadoDA();
+                EmpleadoBE beEmpleado;
+
+                beEmpleado = daEmpleado.SelectById(idEmpleado);
+                return beEmpleado;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
     }
 }
