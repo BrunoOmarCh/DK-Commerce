6077a8d4946de72b5ef7cd3b4d198239f6bc0f1c
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-14 18:48:25 -0500

    Agregar metodo para obtener Pedido por ID

--- Desarrollo/DKCommerceBussinesLogic/PedidoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/PedidoBL.cs
@@ -10,5 +10,21 @@ namespace DKCommerceBussinesLogic
 {
     public class PedidoBL
     {
+        public PedidoBE SelectById(int idPedido)
+        {
+            try
+            {
+                var daPedido = new PedidoDA();
+                PedidoBE bePedido;
+
+                bePedido = daPedido.SelectById(idPedido);
+                return bePedido;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
