5ba4fc2a086822fdb235942b7fc9ee726c385a04
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-14 18:52:15 -0500

    Agregar metodo para obtener Producto por ID

--- Desarrollo/DKCommerceBussinesLogic/ProductoBL.cs
+++ Desarrollo/DKCommerceBussinesLogic/ProductoBL.cs
@@ -10,6 +10,21 @@ namespace DKCommerceBussinesLogic
 {
     public class ProductoBL
     {
+        public ProductoBE SelectById(int idProducto)
+        {
+            try
+            {
+                var daProducto = new ProductoDA();
+                ProductoBE beProducto;
+
+                beProducto = daProducto.SelectById(idProducto);
+                return beProducto;
+            }
+            catch (Exception ex)
+            {       
+                throw ex;
+            }
+        }
 
     }
 }
