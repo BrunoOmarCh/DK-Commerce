3113146a4fc0c88ede957926c9bedf4e6242a5ae
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-23 19:44:51 -0500

    Agregar funcionalidad de edicion para la tabla ContactoProveedor

--- Desarrollo/DKCommerceUI/wwwroot/views/contactoProveedor/editar.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/contactoProveedor/editar.js
@@ -1,32 +1,24 @@
 ﻿"use strict";
 
-function fnUpdate(idProveedor) {
-    let proveedor = {
-        Nombre: $("#NombreTxt").val(),
-        Ruc: $("#RucTxt").val(),
-        ContactoId: $("#ContactoTxt").val(),
-        Direccion: $("#DireccionTxt").val(),
-        Ciudad: $("#CiudadTxt").val(),
-        Region: $("#RegionTxt").val(),
-        CodPostal: $("#CodPostalTxt").val(),
-        Pais: $("#PaisTxt").val(),
-        Telefono: $("#TelefonoTxt").val(),
-        Fax: $("#FaxTxt").val(),
-        PaginaPrincipal: $("#PaginaPrincipalTxt").val()
+function fnUpdate(idContactoProveedor) {
+    let contactoProveedor = {
+        NombreContacto: $("#NombreContactoTxt").val(),
+        CargoContacto: $("#CargoContactoTxt").val(),
+        Dni: $("#DniTxt").val()
     };
 
-    console.log("Datos del proveedor:", proveedor);
-    console.log("URL de la solicitud:", "https://localhost:7220/proveedor/update/" + idProveedor);
+    console.log("Datos del contacto proveedor:", contactoProveedor);
+    console.log("URL de la solicitud:", "https://localhost:7220/contactoProveedor/update/" + idContactoProveedor);
 
     $.ajax({
         type: "PUT",
         async: true,
         cache: false,
-        data: JSON.stringify(proveedor),
+        data: JSON.stringify(contactoProveedor),
         contentType: "application/json",
-        url: "https://localhost:7220/proveedor/update/" + idProveedor,
+        url: "https://localhost:7220/contactoProveedor/update/" + idContactoProveedor,
         success: function () {
-            alert("Se actualizó el proveedor: " + $("#NombreTxt").val());
+            alert("Se actualizó el contacto proveedor: " + $("#NombreContactoTxt").val());
         },
         error: function (xhr, status, error) {
             console.error("Error Status: " + status);
@@ -34,12 +26,10 @@ function fnUpdate(idProveedor) {
             console.error("Response Text: " + xhr.responseText);
             alert("Ocurrió un error al actualizar: " + xhr.responseText);
         }
-
     });
 }
 
 $("#GuardarBtn").on("click", function () {
-    const idProveedor = $("#CodigoTxt").val();
-    fnUpdate(idProveedor);
+    const idContactoProveedor = $("#ContactoProveedorIdTxt").val();
+    fnUpdate(idContactoProveedor);
 });
-
