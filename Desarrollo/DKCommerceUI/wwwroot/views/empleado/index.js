cfec0fc75e581ce66512066bf77620445351630d
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-23 19:49:06 -0500

    Agregar funcionalidad de busqueda y eliminacion para la tabla Empleado

--- Desarrollo/DKCommerceUI/wwwroot/views/empleado/index.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/empleado/index.js
@@ -1,65 +1,54 @@
-﻿"use strict"; // Fuerza a declarar variables
+﻿"use strict";
 
-function fnEliminar(idProveedor) {
+function fnEliminar(empleadoId) {
     $.ajax({
         type: "DELETE",
-        contentType: "application/json; chartset=utf-8",// Trabaja con objetos Json; en formato UTF-8 (soporta tildes)
+        contentType: "application/json; charset=utf-8",
         async: true,
         cache: false,
-        url: "https://localhost:7220/proveedor/delete/" + idProveedor,
+        url: `https://localhost:7220/empleado/delete/${empleadoId}`,
         success: function () {
-            alert("Se eliminó el proveedor de código '" + idProveedor + "', con éxito.");
+            alert(`Se eliminó el empleado con ID: ${empleadoId} con éxito.`);
         },
-        error: function (param1, param2, param3) {
-            console.error("param1", param1);
-            console.error("param2", param2);
-            console.error("param3", param3);
-            alert("No se pudo eliminar el proveedor de código '" + idProveedor + "'.");
+        error: function (xhr) {
+            console.error("Error:", xhr.responseText);
+            alert(`No se pudo eliminar el empleado con ID: ${empleadoId}.`);
         }
     });
 }
 
-function fnGet(idProveedor) {
+function fnGet(empleadoId, nombres) {
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         async: true,
         cache: false,
-        url: "https://localhost:7220/proveedor/select-by-id/" + idProveedor,
+        url: `https://localhost:7220/empleado/select?empleadoId=${empleadoId}&nombres=${nombres}`,
         success: function (data) {
-            console.info("data:", data);
-            var proveedor = "Nombre: " + data.Nombre + "<br>" +
-                "Ruc: " + data.Ruc + "<br>" +
-                "Contacto: " + data.ContactoId + "<br>" +
-                "Direccion: " + data.Direccion + "<br>" +
-                "Ciudad: " + data.Ciudad + "<br>" +
-                "Region: " + data.Region + "<br>" +
-                "CodPostal: " + data.CodPostal + "<br>" +
-                "Pais: " + data.Pais + "<br>" +
-                "Telefono: " + data.Telefono + "<br>" +
-                "Fax: " + data.Fax + "<br>" +
-                "PaginaPrincipal: " + data.PaginaPrincipal;
+            console.info("Datos obtenidos:", data);
+            let resultado = `<strong>ID:</strong> ${data.EmpleadoId} <br>
+                <strong>Nombres:</strong> ${data.Nombres} <br>
+                <strong>Apellido Paterno:</strong> ${data.ApellidoPaterno || "N/A"} <br>
+                <strong>Apellido Materno:</strong> ${data.ApellidoMaterno || "N/A"} <br>
+                <strong>Email:</strong> ${data.Email || "N/A"}`;
 
-            $("#ProveedorResult").html(proveedor);
+            $("#EmpleadoResult").html(resultado);
         },
-        error: function (param1, param2, param3) {
-            console.error("param1", param1);
-            console.error("param2", param2);
-            console.error("param3", param3);
-            alert("No se pudo leer el proveedor de código '" + idProveedor + "'.");
+        error: function (xhr) {
+            console.error("Error:", xhr.responseText);
+            alert("No se pudo obtener el empleado.");
         }
     });
 }
 
 $("#BuscarBtn").on("click", function () {
-    let idProvBuscar = $("#ProveedorBuscarTxt").val();
-
-    fnGet(idProvBuscar);
+    let empleadoId = $("#EmpleadoIdTxt").val();
+    let nombres = $("#NombresTxt").val();
+    fnGet(empleadoId, nombres);
 });
 
 $("#EliminarBtn").on("click", function () {
-    let idProv = $("#ProveedorIdTxt").val();
-
-    fnEliminar(idProv);
+    let empleadoId = $("#EliminarEmpleadoIdTxt").val();
+    fnEliminar(empleadoId);
 });
