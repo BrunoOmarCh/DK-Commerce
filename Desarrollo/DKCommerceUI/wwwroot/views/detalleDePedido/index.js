a9eb2a5bd0d530f0a9b610489ba8fd154fec37fd
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-23 19:46:00 -0500

    Agregar funcionalidad de busqueda y eliminacion para la tabla DetalleDePedido

--- Desarrollo/DKCommerceUI/wwwroot/views/detalleDePedido/index.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/detalleDePedido/index.js
@@ -1,65 +1,58 @@
-﻿"use strict"; // Fuerza a declarar variables
+﻿"use strict";
 
-function fnEliminar(idProveedor) {
+function fnEliminar(pedidoId, productoId) {
     $.ajax({
         type: "DELETE",
-        contentType: "application/json; chartset=utf-8",// Trabaja con objetos Json; en formato UTF-8 (soporta tildes)
+        contentType: "application/json; charset=utf-8",
         async: true,
         cache: false,
-        url: "https://localhost:7220/proveedor/delete/" + idProveedor,
+        url: `https://localhost:7220/detalleDePedido/delete/${pedidoId}/${productoId}`,
         success: function () {
-            alert("Se eliminó el proveedor de código '" + idProveedor + "', con éxito.");
+            alert(`Se eliminó el detalle del pedido (Pedido ID: ${pedidoId}, Producto ID: ${productoId}) con éxito.`);
         },
         error: function (param1, param2, param3) {
-            console.error("param1", param1);
-            console.error("param2", param2);
-            console.error("param3", param3);
-            alert("No se pudo eliminar el proveedor de código '" + idProveedor + "'.");
+            console.error("Error:", param1, param2, param3);
+            alert(`No se pudo eliminar el detalle del pedido (Pedido ID: ${pedidoId}, Producto ID: ${productoId}).`);
         }
     });
 }
 
-function fnGet(idProveedor) {
+function fnGet(pedidoId, productoId) {
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         async: true,
         cache: false,
-        url: "https://localhost:7220/proveedor/select-by-id/" + idProveedor,
+        url: `https://localhost:7220/detalleDePedido/select-by-id/${pedidoId}/${productoId}`,
         success: function (data) {
             console.info("data:", data);
-            var proveedor = "Nombre: " + data.Nombre + "<br>" +
-                "Ruc: " + data.Ruc + "<br>" +
-                "Contacto: " + data.ContactoId + "<br>" +
-                "Direccion: " + data.Direccion + "<br>" +
-                "Ciudad: " + data.Ciudad + "<br>" +
-                "Region: " + data.Region + "<br>" +
-                "CodPostal: " + data.CodPostal + "<br>" +
-                "Pais: " + data.Pais + "<br>" +
-                "Telefono: " + data.Telefono + "<br>" +
-                "Fax: " + data.Fax + "<br>" +
-                "PaginaPrincipal: " + data.PaginaPrincipal;
+            var detalle = `Pedido ID: ${data.PedidoId}<br>
+                Producto ID: ${data.ProductoId}<br>
+                Precio Neto: ${data.PrecioNeto}<br>
+                Cantidad: ${data.Cantidad}<br>
+                Descuento: ${data.Descuento}<br>
+                IGV: ${data.Igv || "N/A"}<br>
+                ISC: ${data.Isc || "N/A"}<br>
+                SubTotal: ${data.MontoSubTotal || "N/A"}`;
 
-            $("#ProveedorResult").html(proveedor);
+            $("#DetalleDePedidoResult").html(detalle);
         },
         error: function (param1, param2, param3) {
-            console.error("param1", param1);
-            console.error("param2", param2);
-            console.error("param3", param3);
-            alert("No se pudo leer el proveedor de código '" + idProveedor + "'.");
+            console.error("Error:", param1, param2, param3);
+            alert(`No se pudo leer el detalle del pedido (Pedido ID: ${pedidoId}, Producto ID: ${productoId}).`);
         }
     });
 }
 
 $("#BuscarBtn").on("click", function () {
-    let idProvBuscar = $("#ProveedorBuscarTxt").val();
-
-    fnGet(idProvBuscar);
+    let pedidoId = $("#PedidoIdTxt").val();
+    let productoId = $("#ProductoIdTxt").val();
+    fnGet(pedidoId, productoId);
 });
 
 $("#EliminarBtn").on("click", function () {
-    let idProv = $("#ProveedorIdTxt").val();
-
-    fnEliminar(idProv);
+    let pedidoId = $("#EliminarPedidoIdTxt").val();
+    let productoId = $("#EliminarProductoIdTxt").val();
+    fnEliminar(pedidoId, productoId);
 });
