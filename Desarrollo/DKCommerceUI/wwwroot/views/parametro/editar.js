ecc97ef6f55bdc274746f45e3153b56bae163f78
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2025-03-24 22:54:21 -0500

    Agregar funcionalidad de edicion para la tabla Parametro

--- Desarrollo/DKCommerceUI/wwwroot/views/parametro/editar.js
+++ Desarrollo/DKCommerceUI/wwwroot/views/parametro/editar.js
@@ -1,45 +1,29 @@
 ﻿"use strict";
 
-function fnUpdate(idProveedor) {
-    let proveedor = {
-        Nombre: $("#NombreTxt").val(),
-        Ruc: $("#RucTxt").val(),
-        ContactoId: $("#ContactoTxt").val(),
-        Direccion: $("#DireccionTxt").val(),
-        Ciudad: $("#CiudadTxt").val(),
-        Region: $("#RegionTxt").val(),
-        CodPostal: $("#CodPostalTxt").val(),
-        Pais: $("#PaisTxt").val(),
-        Telefono: $("#TelefonoTxt").val(),
-        Fax: $("#FaxTxt").val(),
-        PaginaPrincipal: $("#PaginaPrincipalTxt").val()
+function fnUpdate(clave) {
+    let parametro = {
+        Grupo: $("#GrupoTxt").val(),
+        Valor: $("#ValorTxt").val()
     };
-
-    console.log("Datos del proveedor:", proveedor);
-    console.log("URL de la solicitud:", "https://localhost:7220/proveedor/update/" + idProveedor);
 
     $.ajax({
         type: "PUT",
         async: true,
         cache: false,
-        data: JSON.stringify(proveedor),
+        data: JSON.stringify(parametro),
         contentType: "application/json",
-        url: "https://localhost:7220/proveedor/update/" + idProveedor,
+        url: `https://localhost:7220/parametro/update/${clave}`,
         success: function () {
-            alert("Se actualizó el proveedor: " + $("#NombreTxt").val());
+            alert(`Se actualizó el parámetro con clave: ${clave} con éxito.`);
         },
-        error: function (xhr, status, error) {
-            console.error("Error Status: " + status);
-            console.error("Error Thrown: " + error);
-            console.error("Response Text: " + xhr.responseText);
-            alert("Ocurrió un error al actualizar: " + xhr.responseText);
+        error: function (xhr) {
+            console.error("Error:", xhr.responseText);
+            alert("Ocurrió un error al actualizar el parámetro.");
         }
-
     });
 }
 
 $("#GuardarBtn").on("click", function () {
-    const idProveedor = $("#CodigoTxt").val();
-    fnUpdate(idProveedor);
+    const clave = $("#ClaveTxt").val();
+    fnUpdate(clave);
 });
-
